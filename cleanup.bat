ce50f2283d03dda92281aa4777772f03cc2c732c
Aviaplanes <h3roinsex@gmail.com>
2026-02-25 19:01:30 +0300

    Remove obsolete install_browser.bat cleanup from post-install script

--- cleanup.bat
+++ cleanup.bat
@@ -2,7 +2,6 @@
 :: Removes files and folders that are no longer needed after setup:
 @echo off
 rmdir /s /q icons 2>nul
-del /f /q install_browser.bat 2>nul
 del /f /q requirements.txt 2>nul
 del /f /q install.bat 2>nul
 del /f /q CHANGELOG.md 2>nul
